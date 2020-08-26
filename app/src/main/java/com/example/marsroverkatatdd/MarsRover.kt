b4f424060aa1c76b664eaef924c3f0e00cc35be3
Faiza-Mazhar <faiza.mazhar@bbc.co.uk>
2020-08-26 08:56:31 +0100

    Initial Commit

---
+++ app/src/main/java/com/example/marsroverkatatdd/MarsRover.kt
@@ -0,0 +1,17 @@
+package com.example.marsroverkatatdd
+
+class MarsRover {
+
+
+}
+
+
+data class Position(val x: Int, val y: Int)
+
+
+enum class Direction{
+    North,
+    South,
+    East,
+    West
+}
